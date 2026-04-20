6823cf55784d118cf80544543d1744260d923aef
2017000014-DiegoLopez <d.lopez.5bdb@gmail.com>
2026-04-20 13:51:54 -0600

    Add files via upload

---
+++ SolucionGuia3DiegoLopez.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solucion.guia3.diegolopez;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class SolucionGuia3DiegoLopez {
+
+    public static void main(String[] args) {
+       jfrmMensajes sofia=new jfrmMensajes();
+       sofia.setVisible(true);
+    }
+}
